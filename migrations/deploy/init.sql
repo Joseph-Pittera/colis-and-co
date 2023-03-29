38dc21664ffe1d2ec4f27ce40e5bc3692d4b9f73
Carlos LOPES <daniel.marqueslopes74@gmail.com>
2023-03-29 10:28:46 +0200

    sqitch configuration

---
+++ migrations/deploy/init.sql
@@ -0,0 +1,7 @@
+-- Deploy colisandco:init to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ migrations/verify/init.sql
@@ -0,0 +1,7 @@
+-- Verify colisandco:init on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
