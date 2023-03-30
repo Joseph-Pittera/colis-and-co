859ff55f6200f049d90d5bf21ceb38415a87226a
Carlos LOPES <daniel.marqueslopes74@gmail.com>
2023-03-30 15:27:26 +0200

    Creating some files, and make some routers website,api

--- app/routers/website/index.js
+++ app/routers/website/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
-const websiteControllers = require('../controllers/website');
+const websiteController = require('../../controllers/website');
 const websiteErrorHandler = require('../../errors/helpers/websiteErrorHandler');
 const NoResourceFoundError = require('../../errors/NoResourceFoundError');
+
+const router = express.Router();
+
+router.get('/', websiteController.getHome);
+
+router.use((_, res, next) => {
+  next(new NoResourceFoundError());
+});
+
+router.use(websiteErrorHandler);
+
+module.exports = router;
