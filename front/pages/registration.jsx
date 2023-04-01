15167a92076149b7a07028461488cfb1430db4f7
jopit59 <pittera.joseph@gmail.com>
2023-04-01 08:47:45 +0200

    register form finished

--- front/pages/registration.jsx
+++ front/pages/registration.jsx
@@ -1,5 +1,5 @@
 import { Layout } from "@/components/Layout";
-import { TextField, Typography, Stack } from "@mui/material";
+import { TextField, Typography, Stack, Box } from "@mui/material";
 import { ConnexionBox } from "@/components/Connexion/ConnexionBox";
 import { MainButton } from "@/components/CustomsMuiComp/MainButton";
 import Link from "next/link";
@@ -20,13 +20,72 @@ export default function Registration() {
         Formulaire d'inscription
       </Typography>
       <ConnexionBox>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="flex-start"
-          flexWrap="wrap"
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
         >
-          {/* {connexionDatas.map((data, i) => (
+          <Stack
+            direction="column"
+            m={1}
+            sx={{ minWidth: "20rem", maxWidth: "30rem", width: "45%" }}
+          >
+            <Typo>Nom :</Typo>
+            <TextField required placeholder="Nom" sx={{ maxWidth: "16rem" }} />
+            <Typo>Prénom :</Typo>
+            <TextField
+              required
+              placeholder="Prénom"
+              sx={{ maxWidth: "16rem" }}
+            />
+            <Typo>Date de naissance :</Typo>
+            <TextField required type="date" sx={{ maxWidth: "16rem" }} />
+            <Typo>Téléphone :</Typo>
+            <TextField
+              required
+              placeholder="Téléphone"
+              type="tel"
+              sx={{ maxWidth: "16rem" }}
+            />
+          </Stack>
+          <Stack
+            direction="column"
+            m={1}
+            sx={{ minWidth: "20rem", maxWidth: "30rem", width: "45%" }}
+          >
+            <Typo>Adresse :</Typo>
+            <TextField required placeholder="Adresse" />
+            <Typo>Complément d'adresse :</Typo>
+            <TextField placeholder="Adresse2" />
+            <Stack direction="row">
+              <Stack direction="column">
+                <Typo>Ville :</Typo>
+                <TextField required placeholder="Ville" />
+              </Stack>
+              <Stack direction="column">
+                <Typo>Code postal :</Typo>
+                <TextField required placeholder="Code postal" />
+              </Stack>
+            </Stack>
+          </Stack>
+        </Box>
+      </ConnexionBox>
+      <Stack spacing={2} direction="row">
+        <Link href="/">
+          <MainButton>Valider</MainButton>
+        </Link>
+        <Link href="/">
+          <MainButton>Annuler</MainButton>
+        </Link>
+      </Stack>
+    </Layout>
+  );
+}
+
+{
+  /* {connexionDatas.map((data, i) => (
             <Stack
               key={`${data}-${i}`}
               direction="column"
@@ -55,53 +114,5 @@ export default function Registration() {
                 />
               )}
             </Stack>
-          ))} */}
-
-          <Stack direction="column" m={1} sx={{ maxWidth: "30rem" }}>
-            <Typo>Nom :</Typo>
-            <TextField required placeholder="Nom" sx={{ maxWidth: "16rem" }} />
-            <Typo>Prénom :</Typo>
-            <TextField
-              required
-              placeholder="Prénom"
-              sx={{ maxWidth: "16rem" }}
-            />
-            <Typo>Date de naissance :</Typo>
-            <TextField required type="date" sx={{ maxWidth: "16rem" }} />
-            <Typo>Téléphone :</Typo>
-            <TextField
-              required
-              placeholder="Téléphone"
-              type="tel"
-              sx={{ maxWidth: "16rem" }}
-            />
-          </Stack>
-          <Stack direction="column" m={1} sx={{ maxWidth: "30rem" }}>
-            <Typo>Adresse :</Typo>
-            <TextField required placeholder="Adresse" />
-            <Typo>Complément d'adresse :</Typo>
-            <TextField placeholder="Adresse2" />
-            <Stack direction="row">
-              <Stack direction="column">
-                <Typo>Ville :</Typo>
-                <TextField required placeholder="Ville" />
-              </Stack>
-              <Stack direction="column">
-                <Typo>Code postal :</Typo>
-                <TextField required placeholder="Code postal" />
-              </Stack>
-            </Stack>
-          </Stack>
-        </Stack>
-      </ConnexionBox>
-      <Stack spacing={2} direction="row">
-        <Link href="/">
-          <MainButton>Valider</MainButton>
-        </Link>
-        <Link href="/">
-          <MainButton>Annuler</MainButton>
-        </Link>
-      </Stack>
-    </Layout>
-  );
+          ))} */
 }
