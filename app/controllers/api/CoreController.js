730767cf960d626c9ac992bb25286bd9b7ef7af4
Lassaad TOUIBI <touibilassaad@hotmail.fr>
2023-04-02 06:25:57 +0200

    I added some code comments to CoreController.js

--- app/controllers/api/CoreController.js
+++ app/controllers/api/CoreController.js
@@ -1,15 +1,27 @@
+//Import the 'debug' function from the 'debug' module and set the namespace to 'colis: controllers' 'https://www.npmjs.com/package/debug'
 const debug = require('debug')('colis:controllers');
 
 /** Class representing an abstract core controller. */
 class CoreController {
   static dataMapper;
 
+  /**
+   * Async function to get all records from a table and return them as JSON
+   * @param {*} _ 
+   * @param {*} response 
+   */
   async getAll(_, response) {
     debug(`${this.constructor.name} getAll`);
     const results = await this.constructor.dataMapper.findAll();
     response.json(results);
   }
 
+  /**
+   * Async function to get one record by ID and return it as JSON or a 204 status if not found
+   * @param {*} request 
+   * @param {*} response 
+   * @returns 
+   */
   async getOne(request, response) {
     debug(`${this.constructor.name} getOne`);
     const { id } = request.params;
@@ -20,12 +32,22 @@ class CoreController {
     return response.status(204).send();
   }
 
+  /**
+   * Async function to create a new record in the table and return it as JSON
+   * @param {*} request 
+   * @param {*} response 
+   */
   async create(request, response) {
     debug(`${this.constructor.name} create`);
     const results = await this.constructor.dataMapper.create(request.body);
     response.json(results);
   }
 
+  /**
+   * Async function to modify an existing record by ID and return the updated record as JSON
+   * @param {*} request 
+   * @param {*} response 
+   */
   async modify(request, response) {
     debug(`${this.constructor.name} modify`);
     const { id } = request.params;
@@ -33,6 +55,12 @@ class CoreController {
     response.json(results);
   }
 
+  /**
+   * Async function to delete a record by ID and return a 204 status.
+   * @param {*} request 
+   * @param {*} response 
+   * @returns 
+   */
   async delete(request, response) {
     debug(`${this.constructor.name} delete`);
     const { id } = request.params;
