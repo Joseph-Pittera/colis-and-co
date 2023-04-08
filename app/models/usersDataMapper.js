5c1bdaf471d5c785c34d481c602067e5f0917e02
Carlos LOPES <daniel.marqueslopes74@gmail.com>
2023-04-08 21:14:41 +0200

    Trying to have a unique email

--- app/models/usersDataMapper.js
+++ app/models/usersDataMapper.js
@@ -62,7 +62,18 @@ class UserDataMapper extends CoreDataMapper {
     };
   }
 
+  async findByEmail(email) {
+    debug(`${this.constructor.name} findByEmail`);
+    const preparedQuery = {
+      text: `SELECT * FROM ${this.constructor.tableName} WHERE email = $1`,
+      values: [email],
+    };
+    const results = await client.query(preparedQuery);
+    return results.rows[0];
+  }
+
   async createSecureUser(newUser) {
+    debug(`${this.constructor.name} CreateSecureUser`);
     // Hasher le mot de passe avant de l'ajouter à la base de données
     const hashedPassword = await bcrypt.hash(newUser.password, saltRounds);
 
