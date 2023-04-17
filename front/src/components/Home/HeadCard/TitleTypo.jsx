bd81966dfd5e9ad37453a133d4c8015fe6dfba16
jopit59 <pittera.joseph@gmail.com>
2023-04-17 14:13:50 +0200

    reset front

--- front/src/components/Home/HeadCard/TitleTypo.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export const TitleTypo = ([text, tag, size]) => {
-  return (
-    <Typography component={tag} fontSize={size}>
-      {text}
-    </Typography>
-  );
-};
