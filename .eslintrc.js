d7a453e3d9e41cd531b2a4ecde4f836c8dc0849e
Lassaad TOUIBI <touibilassaad@hotmail.fr>
2023-04-17 15:12:10 +0200

    JEST test with error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
@@ -17,5 +18,10 @@ module.exports = {
     'react',
   ],
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
